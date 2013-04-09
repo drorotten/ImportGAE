8e49ed5b0271e53f0bd1b54be7c828608b5af60a
dror.otten@gmail.com <>
2013-04-09 12:11:12 +0000

    Basic servlet with Google User login and Simple DB

---
+++ src/main/java/dashboard/ImportServlet.java
@@ -0,0 +1,54 @@
+package dashboard;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ImportServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(ImportServlet.class.getName());
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+                throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        String content = req.getParameter("content");
+        if (content == null) {
+            content = "(No post)";
+        }
+        if (user != null) {
+            log.info("Posted by user " + user.getNickname() + ": " + content);
+        } else {
+            log.info("Posted anonymously: " + content);
+        }
+
+        String guestbookName = req.getParameter("guestbookName");
+        Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
+        Date date = new Date();
+        
+        Entity greeting = new Entity("Greeting", guestbookKey);
+        greeting.setProperty("user", user);
+        greeting.setProperty("date", date);
+        greeting.setProperty("content", content);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(greeting);
+
+        resp.sendRedirect("/display.jsp?guestbookName=" + guestbookName);
+
+    }
+
+}
